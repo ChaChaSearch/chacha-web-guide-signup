1ad2e529692a94e1b92a5222e1aeed97fe5bb32b
Kyle Brown <kyle.brown@chacha.com>
2015-07-15 10:09:18 -0400

    initial projec template

---
+++ src/com/chacha/guide/signup/routing/ApplicationRoutes.java
@@ -0,0 +1,16 @@
+package com.chacha.guide.signup.routing;
+
+import com.chacha.camel.routing.ChaRoute;
+
+/**
+ * Core Application Routes
+ *
+ */
+public class ApplicationRoutes extends ChaRoute
+{
+	@Override
+	public void configure() throws Exception
+	{
+
+	}
+}
